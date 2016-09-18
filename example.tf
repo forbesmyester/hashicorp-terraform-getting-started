6a95445229b0f9ae30b3da26f69d5f3bcfb778ae
Matthew Forrester <matthew.forrester@speechmarks.com>
2016-09-18 11:27:22 +0100

    convert to aws profile

--- example.tf
+++ example.tf
@@ -1,10 +1,8 @@
 // You cannot use env vars directly, for example in provider "aws". You need to have these variables up here.
-variable "aws_access_key_id" {}
-variable "aws_secret_access_key" {}
+variable "aws_profile" {}
 
 provider "aws" {
-    access_key = "${var.aws_access_key_id}"
-    secret_key = "${var.aws_secret_access_key}"
+    profile = "${var.aws_profile}"
     region = "us-east-1"
 }
 
